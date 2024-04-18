f59deaf7b1735c7e6f3db5a273fea63bca6d34db
DouglasAmos <douglas.amos1@gmail.com>
2024-04-17 20:10:13 -0400

    finished nav, footer animation.  Layout good

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
 let navbarHeight = navbar.offsetHeight;
+const footer = document.getElementById("footer");
+let footerHeight = footer.offsetHeight;
 
 window.addEventListener("scroll", function() {
     let scrollTop = document.documentElement.scrollTop;
@@ -8,10 +10,14 @@ window.addEventListener("scroll", function() {
     if (scrollTop > lastScrollTop) {
         // Scroll down
         navbar.classList.add("hidden");
+        footer.classList.add("hidden");
     } else {
         // Scroll up
         navbar.classList.remove("hidden");
+        footer.classList.remove("hidden");
     }
     
     lastScrollTop = scrollTop;
 });
+
+
