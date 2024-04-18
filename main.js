467e6dd7f500140b72fe272abf1d9b97dc28f9ed
DouglasAmos <douglas.amos1@gmail.com>
2024-04-17 22:10:52 -0400

    working on mobile view appearence

--- main.js
+++ main.js
@@ -4,20 +4,18 @@ let navbarHeight = navbar.offsetHeight;
 const footer = document.getElementById("footer");
 let footerHeight = footer.offsetHeight;
 
-window.addEventListener("scroll", function() {
-    let scrollTop = document.documentElement.scrollTop;
+window.addEventListener("scroll", function () {
+  let scrollTop = document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-        // Scroll down
-        navbar.classList.add("hidden");
-        footer.classList.add("hidden");
-    } else {
-        // Scroll up
-        navbar.classList.remove("hidden");
-        footer.classList.remove("hidden");
-    }
-    
-    lastScrollTop = scrollTop;
+  if (scrollTop > lastScrollTop) {
+    // Scroll down
+    navbar.classList.add("hidden");
+    footer.classList.add("hidden");
+  } else {
+    // Scroll up
+    navbar.classList.remove("hidden");
+    footer.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop;
 });
-
-
